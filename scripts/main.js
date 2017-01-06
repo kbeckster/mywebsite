529baa46fdb2c620604f2bf59e6192de921d268e
kbeckster <kohl.rebecca@gmail.com>
2017-01-05 21:07:22 -0600

    removing uneeded code

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,3 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
-
-
-//STYLING
-//$('.text-section').viewportChecker({
-//    classToAdd: 'animated slideInLeft',
-//    classToRemove: 'hidden'
-//});
-
 $('.work-img').hover(
     function () {
         $(this).addClass('animated pulse')
