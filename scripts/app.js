1b5a46862d80b5f0a7cbed52603ba2bceecbbaad
kbeckster <kohl.rebecca@gmail.com>
2017-05-04 21:14:11 -0500

    fixing views

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+/**
+ * @ngdoc overview
+ * @name mysiteApp
+ * @description
+ * # mysiteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mysiteApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+      })
+      .when ('/about', {
+        templateUrl: 'views/main.html',
+      })
+      .when('/latestproject', {
+        templateUrl: 'views/latestProject.html',
+        controller: 'LatestProjectCtrl',
+        controllerAs: 'latestproject'
+      })
+      .when('/favbooks', {
+        templateUrl: 'views/favBooks.html',
+        controller: 'FavBooksCtrl',
+        controllerAs: 'favbooks'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
