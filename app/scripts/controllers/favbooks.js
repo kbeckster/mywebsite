fb8989384a9058e32271224f9fc3219719114a76
kbeckster <kohl.rebecca@gmail.com>
2017-05-17 22:15:42 -0500

    changed book list from text to pics and added some styling

--- app/scripts/controllers/favbooks.js
+++ app/scripts/controllers/favbooks.js
@@ -9,10 +9,9 @@
 
 angular.module('mysiteApp')
     .controller('FavBooksCtrl', function($scope, $http) {
-
-      $http.get('https://www.googleapis.com/books/v1/users/100652730531308870109/bookshelves/0/volumes?key=AIzaSyCShvpu3o-5xpLHqfOOn6VBejqtx7yNRUw')
+      //Call to get "Favorites" library from Google Books. Max amount you can retrieve is 40
+      $http.get('https://www.googleapis.com/books/v1/users/100652730531308870109/bookshelves/0/volumes?key=AIzaSyCShvpu3o-5xpLHqfOOn6VBejqtx7yNRUw&maxResults=40')
       .then(function(response) {
           $scope.books = response.data.items;
       });
-
   });
